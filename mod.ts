569586e51047a3c83e63b73365d419435731ab51
Helloyunho <yunho050840@gmail.com>
2021-04-16 13:01:52 +0900

    :sparkles: Add mod.ts for importing

---
+++ mod.ts
@@ -0,0 +1,13 @@
+export { OpenAI } from './src/mod.ts'
+export type { AnswerArgs, AnswerResponse } from './src/types/answer.ts'
+export type {
+  ClassificationArgs,
+  ClassificationResponse
+} from './src/types/classification.ts'
+export type {
+  CompletionArgs,
+  CompletionResponse
+} from './src/types/completion.ts'
+export type { EngineResponse, EnginesResponse } from './src/types/engine.ts'
+export type { File } from './src/types/File.ts'
+export type { SearchArgs, SearchResponse } from './src/types/search.ts'
